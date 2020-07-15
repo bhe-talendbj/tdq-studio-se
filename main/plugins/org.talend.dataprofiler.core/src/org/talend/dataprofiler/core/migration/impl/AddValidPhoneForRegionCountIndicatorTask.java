600883ba0248b6faadca83d3209c12b7a722e2f8
qiongli <qiongli@talend.com>
2020-07-15 18:46:15 +0800

    feat(TDQ-18439):Improve the migration task

--- main/plugins/org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/migration/impl/AddValidPhoneForRegionCountIndicatorTask.java
+++ main/plugins/org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/migration/impl/AddValidPhoneForRegionCountIndicatorTask.java
@@ -15,15 +15,14 @@ package org.talend.dataprofiler.core.migration.impl;
 import java.util.Date;
 
 import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.Path;
 import org.talend.dataprofiler.core.CorePlugin;
 import org.talend.dataprofiler.core.manager.DQStructureManager;
 import org.talend.dataprofiler.core.migration.AbstractWorksapceUpdateTask;
 import org.talend.resource.ResourceManager;
 
-public class AddValidPhoneForRegionCountIndicator extends AbstractWorksapceUpdateTask {
-
-    private final String INDICATORNAME = "Valid Phone Number For Region Count";//$NON-NLS-1$
+public class AddValidPhoneForRegionCountIndicatorTask extends AbstractWorksapceUpdateTask {
 
     private final String SRCFOLDERPATH = "/indicators/Phone Number Statistics/"; //$NON-NLS-1$
 
@@ -41,7 +40,15 @@ public class AddValidPhoneForRegionCountIndicator extends AbstractWorksapceUpdat
     protected boolean doExecute() throws Exception {
         DQStructureManager manager = DQStructureManager.getInstance();
         IFolder desFolder = ResourceManager.getSystemIndicatorFolder().getFolder("Phone Number Statistics");
-        if (!desFolder.getFile(INDICATORNAME).exists()) {
+        boolean isExist = false;
+        for (IResource res : desFolder.members()) {
+            if (res.getName().startsWith("Valid_Phone_Number_For_Region_Count")) {
+                isExist = true;
+                break;
+            }
+        }
+        if (!isExist) {
+
             manager
                     .copyFilesToFolder(CorePlugin.getDefault(), new Path(SRCFOLDERPATH).toString(), false, desFolder,
                             null);
